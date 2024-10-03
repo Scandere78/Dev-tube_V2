d5e23a93b794027036f45cdfd3359d5c4ed9f144
ScandereT78 <scanderetejpro@gmail.com>
2024-10-03 15:05:04 +0100

    test

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Assurez-vous que les fichiers à surveiller par Tailwind sont inclus
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
